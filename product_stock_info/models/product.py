a80093871672dad67dbb398dfcee581bc3c0dfde
Ismael Calvo <ismael.calvo@factorlibre.com>
2016-02-10 17:15:37 +0100

    [FIX] product_stock_info field_names

--- product_stock_info/models/product.py
+++ product_stock_info/models/product.py
@@ -30,8 +30,9 @@ class ProductProduct(models.Model):
                                       compute='_product_available')
 
     @api.multi
-    def _product_available(self):
-        res = super(ProductProduct, self)._product_available()
+    def _product_available(self, field_names=None, arg=False):
+        res = super(ProductProduct, self)._product_available(
+            field_names=None, arg=False)
         for product in self:
             qty = res[product.id]
             qty['real_qty_available'] = \
@@ -47,9 +48,9 @@ class ProductTemplate(models.Model):
                                       compute='_product_available')
 
     @api.multi
-    def _product_available(self, name=False, arg=False):
-        res = super(ProductTemplate, self)._product_available(name=name,
-                                                              arg=arg)
+    def _product_available(self, field_names=None, arg=False):
+        res = super(ProductTemplate, self)._product_available(
+            field_names=None, arg=False)
         for product in self:
             qty = res[product.id]
             qty['real_qty_available'] = \
